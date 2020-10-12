79da99f6f2fb0bf418bc1e283ba6cc164d58d5b7
RicardoReyes44 <rickyboy1002@gmail.com>
2020-10-12 15:07:09 -0500

    Se agrego la clase 'ImplementacionPilaEstatica', se le agregaron los metodos 'verificarPilaLLena' y 'verificarPilaVacia' y se implemento la interfaz 'RentaPeliculas'

---
+++ src/ImplementacionPilaEstatica.java
@@ -0,0 +1,37 @@
+
+public class ImplementacionPilaEstatica implements RentaPeliculas{
+
+	@Override
+	public Pelicula eliminar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pelicula obtenerUltimo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean agregar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int obtenerTamaño() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private boolean verificarPilaLlena() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private boolean verificarPilaVacia() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
--- src/RentaPeliculas.java
+++ src/RentaPeliculas.java
@@ -1,11 +1,9 @@
 
 public interface RentaPeliculas {
-	
+
 	public Pelicula eliminar();
 	public Pelicula obtenerUltimo();
 	public boolean agregar();
 	public int obtenerTamaño();
-	public boolean verificarPilaLlena();
-	public boolean verificarPilaVacia();
-	
+
 }
