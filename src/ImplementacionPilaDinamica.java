cbe5d5be04d5c92c269a15b1b21589314acf14d6
RicardoReyes44 <rickyboy1002@gmail.com>
2020-10-13 21:48:03 -0500

    Se agrego la clase 'ImplementacionPilaDInamica' ademas se le implemento la interfaz 'RentaPeliculas' y se le agrego el atributo 'pila'

--- src/ImplementacionPilaDinamica.java
+++ src/ImplementacionPilaDinamica.java
@@ -1,4 +1,44 @@
+import java.util.Stack;
 
-public class ImplementacionPilaDinamica {
+public class ImplementacionPilaDinamica implements RentaPeliculas{
+	
+	Stack<Pelicula> pila;
+	
+	public ImplementacionPilaDinamica() {pila = new Stack<Pelicula>();}
+	
+	@Override
+	public Pelicula eliminar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public Pelicula obtenerUltimo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean agregar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int obtenerTamaño() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean verificarPilaLlena() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean verificarPilaVacia() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
